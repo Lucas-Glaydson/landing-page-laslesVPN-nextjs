a223226cf4fe8cb60f24e458b4f983b3f9a950fc
lucas glaydson <lucas.glaydson01@hotmail.com>
2024-12-17 11:11:51 -0300

    feat: add lefthook and commitlint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "lefthook.yml",
+      "package.json",
+      "package-lock.json",
+      "commitlint.config.js",
+    ],
+  },
 ];
 
 export default eslintConfig;
