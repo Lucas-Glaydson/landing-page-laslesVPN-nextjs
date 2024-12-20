a90b7e8244f82b4193691d8ca7a03a0eeb26d723
lucas glaydson <lucas.glaydson01@hotmail.com>
2024-12-20 12:21:47 -0300

    fix: solving carroussel comments

--- src/components/Main/customer/Customer.tsx
+++ src/components/Main/customer/Customer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Carousel,
   CarouselContent,
@@ -6,10 +8,11 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import CustomerCard from "./CustomerCard";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function Customer() {
   return (
-    <section className="font-rubik text-center mx-2">
+    <section className="font-rubik text-center mx-2 overflow-hidden">
       <div className="flex flex-col justify-center items-center">
         <h2 className="font-semibold text-xl w-56 text-center mb-3">
           Trusted by thousands of happy customer
@@ -19,28 +22,35 @@ export default function Customer() {
           Vel vel erat semper augue.
         </p>
       </div>
-      <div className="flex flex-col items-center ">
+      <div >
         <Carousel
           opts={{
             align: "center",
           }}
-          className="w-full max-w-5xl"
+          className="w-full max-w-7xl m-auto"
+          plugins={[
+            Autoplay({
+              delay: 8000,
+            }),
+          ]}
         >
           <CarouselContent>
-            {Array.from({ length: 3 }).map((_, index) => (
-              <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/3">
-                  <CustomerCard
-                    imageUrl="/man.svg"
-                    name="Brooklyn Simmons"
-                    description="Warsaw, Poland"
-                    rate="4.5"
-                    text="“Wow... I am very happy to use this VPN. It turned out to be more than my expectations, and so far, there have been no problems. LaslesVPN is always the best.”"
-                  />
+            {Array.from({ length: 4 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/3 lg:basis-1/3 basis-11/12">
+                <CustomerCard
+                  imageUrl="/man.svg"
+                  name="Brooklyn Simmons"
+                  description="Warsaw, Poland"
+                  rate="4.5"
+                  text="“Wow... I am very happy to use this VPN. It turned out to be more than my expectations, and so far, there have been no problems. LaslesVPN is always the best.”"
+                />
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="px-4 py-2 bg-gray-300 text-black rounded-full hover:bg-gray-400" />
-          <CarouselNext className="px-4 py-2 bg-red text-white rounded-full hover:bg-red-600 ml-4" />
+          <div className="flex justify-center mt-4">
+            <CarouselPrevious className="px-4 py-2 bg-gray-400 text-white rounded-full hover:bg-gray-500 mx-2" />
+            <CarouselNext className="px-4 py-2 bg-red text-white rounded-full hover:bg-red-600 mx-2" />
+          </div>
         </Carousel>
       </div>
     </section>
