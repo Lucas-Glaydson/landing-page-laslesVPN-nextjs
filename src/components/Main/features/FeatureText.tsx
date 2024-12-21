9034168fbb2a72ccec680c019b782ce8e63e67e6
lucas glaydson <lucas.glaydson01@hotmail.com>
2024-12-21 08:57:07 -0300

    fix: features section to desktop version

--- src/components/Main/features/FeatureText.tsx
+++ src/components/Main/features/FeatureText.tsx
@@ -4,7 +4,7 @@ export default function FeatureText({text}: {text: string}){
   return (
       <li className="flex justify-start items-center mb-5 space-x-2">
         <Image src="/tick-circle.svg" alt="correct image" width={15} height={15}/>
-        <span className="text-xs font-semibold">
+        <span className="text-xs font-semibold lg:text-base">
           {text}
         </span>
       </li>
--- src/components/Main/features/Features.tsx
+++ src/components/Main/features/Features.tsx
@@ -3,16 +3,20 @@ import FeatureText from "./FeatureText";
 
 export default function Features(){
   return (
-    <section className="m-auto flex flex-col items-center mx-6 mt-10">
-      <Image src="/Illustration2.svg" alt="Second guy using pc" width={275} height={224}/>
-      <h2 className="text-xl font-semibold mt-6 text-balance">We provide many features you can use</h2>
-      <p className="text-sm mt-5 text-left">Lorem ipsum dolor sit amet consectetur. Quis tortor gravida nibh arcu id purus ullamcorper. Vel vel erat semper augue.</p>
-      <ul className="w-full mt-8">
-        <FeatureText text="Powerfull online protection."/>
-        <FeatureText text="Internet with borders."/>
-        <FeatureText text="Supercharged VPN."/>
-        <FeatureText text="No specific time limits."/>
-      </ul>
+    <section className="m-auto flex flex-col items-center mx-6 mt-10 lg:flex-row lg:justify-center lg:gap-36">
+      <div>
+        <Image className="lg:w-[510px] lg:h-[416px]" src="/Illustration2.svg" alt="Second guy using pc" width={275} height={224}/>
+      </div>
+      <div className="lg:w-96">
+        <h2 className="text-xl font-semibold mt-6 text-balance lg:text-[30px]">We provide many features you can use</h2>
+        <p className="text-sm mt-5 text-left lg:text-base">Lorem ipsum dolor sit amet consectetur. Quis tortor gravida nibh arcu id purus ullamcorper. Vel vel erat semper augue.</p>
+        <ul className="w-full mt-8">
+          <FeatureText text="Powerfull online protection."/>
+          <FeatureText text="Internet with borders."/>
+          <FeatureText text="Supercharged VPN."/>
+          <FeatureText text="No specific time limits."/>
+        </ul>
+      </div>
     </section>
   )
 }
