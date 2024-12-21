98afd6bc8ea0ead2e3d7a6122cbe3687fc904ee5
lucas glaydson <lucas.glaydson01@hotmail.com>
2024-12-21 09:57:23 -0300

    feat: create subscribe section for desktop

--- src/components/Main/Subscribe.tsx
+++ src/components/Main/Subscribe.tsx
@@ -1,7 +1,7 @@
 export default function Subscribe(){
   return(
-    <section className="shadow-md w-[350px] m-auto h-80 text-center rounded-md font-rubik p-4 flex items-center flex-col justify-around">
-      <div>  
+    <section className="shadow-md w-[350px] m-auto h-80 text-center rounded-xl font-rubik p-4 flex items-center flex-col justify-around lg:w-[1146px] lg:h-[181px] lg:flex-row lg:justify-between lg:px-14">
+      <div className="lg:w-64 lg:text-start">  
         <h1 className="text-2xl text-[#5A5A5A] font-semibold mb-2">Subscribe now for get special features!</h1>
         <p>Lorem ipsum dolor sit amet </p>
       </div>
