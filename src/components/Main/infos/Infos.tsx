953a92aac7d91323aa621e9df0d8f19e2d212615
lucas glaydson <lucas.glaydson01@hotmail.com>
2024-12-21 08:36:49 -0300

    feat: infos in pc version

--- src/components/Main/infos/Infos.tsx
+++ src/components/Main/infos/Infos.tsx
@@ -2,11 +2,11 @@ import InfoData from "./InfoData";
 
 export default function Infos() {
   return (
-    <section className="flex shadow-xl w-[331px] h-16 justify-around rounded-xl m-auto items-center py-11 my-8">
+    <section className="flex shadow-xl w-[331px] h-16 justify-around rounded-xl m-auto items-center py-11 my-8 lg:w-[820px] lg:h-[114px]">
       <InfoData image="/icons/user.svg" title="Users" value="90+" />
-      <div className="h-[50%] bg-black text-black">|</div>
+      <div className="h-[50%] text-black">|</div>
       <InfoData image="/icons/location.svg" title="Location" value="90+" />
-      <div className="h-[50%] bg-bla text-black">|</div>
+      <div className="h-[50%] text-black">|</div>
       <InfoData image="/icons/servers.svg" title="Servers" value="90+" />
     </section>
   );
